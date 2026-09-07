1e5cbfa8f18568b5e6c5f5190f121afbec27b80c
LCH <cogusdla1112@gmail.com>
2026-09-07 11:04:41 +0900

    first push

---
+++ src/main/java/com/aio/hospitalsafety/HospitalSafetyApplication.java
@@ -0,0 +1,13 @@
+package com.aio.hospitalsafety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSafetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalSafetyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aio/hospitalsafety/HospitalSafetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aio.hospitalsafety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSafetyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
